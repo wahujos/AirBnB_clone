a72044427547a1a87e393a7c587155086b0d4e18
AtoZconcept <opeyemiolaafeez.com>
2023-12-11 08:40:51 +0100

    add

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """definig str that print"""
